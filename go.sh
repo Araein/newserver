99fa3c05ecf2a2de1540b26a2899fb6d359294a8
Araein <75018476+Araein@users.noreply.github.com>
2021-06-29 21:59:20 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+docker stop monconteneur
+docker rm monconteneur
+docker image rm monimage
 docker build -t monimage .
 docker run -d -p 80:80 --name=monconteneur monimage
 docker exec -ti monconteneur bash
