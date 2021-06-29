cad83a6dc3757cfff33e3bf967c4ddee78ae95a2
Araein <75018476+Araein@users.noreply.github.com>
2021-06-29 22:02:09 +0200

    Create wp-config.php

---
+++ srcs/wp-config.php
@@ -0,0 +1,40 @@
+<?php
+
+/**https://wordpress.org/support/article/editing-wp-config-php/#default-wp-config-sample-php*/
+
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+/** MySQL database username */
+define( 'DB_USER', 'wordpress_user' );
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' )
+
+
+
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+
+define( 'AUTH_KEY',         't`DK%X:>xy|randomizethis8#~UzUQG-^_Cs_GHs5U-&Wb?pgn^p8(2@}IcnCa|' );
+define( 'SECURE_AUTH_KEY',  'D&ovlU#|CvJ##uNq}bel+^MFtT&.b9{UvR]g%randomizethis7q!h}XWdEC[BOKXssj' );
+define( 'LOGGED_IN_KEY',    'MGKi8Br(&{H*~0s;{k0<SrandomizethispJ-+P;RDKT:~jrmgj#/-,[hOBk!ry^' );
+define( 'NONCE_KEY',        'FIsAsXJKL5ZlQo)iD-pt??eUbdc{_Cn<4!d~randomizethisAwK%)+)F2aNwI|siOe' );
+define( 'AUTH_SALT',        '7T-!^i!0,w)L#JK@pc2{8XE[DenYI^BVrandomizethisf{L:jvF,hf}zBf883td6D;Vcy8,S)-&G' );
+define( 'SECURE_AUTH_SALT', 'I6`V|mDZq21-J|randomizethisihb u^q0F }F_NUcy`l,=obGtq*p#Ybe4a31R,r=|n#=]@]c #' );
+define( 'LOGGED_IN_SALT',   'w<$4c$Hmd%/*]`Oomrandomizethis>(hdXW|0M=X={we6;Mpvtg+V.o<$|#_}qG(GaVDEsn,~*4i' );
+define( 'NONCE_SALT',       'a|#h{c5|P &xWs4IZ20c2&%4!c(/uG}W:mAvyI44`jAburandomizethisp]t]V`}.py(wTP%%' );
+  
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+
+  if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
+  
