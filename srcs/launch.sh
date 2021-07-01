53a2415b1b801a69545f84135326ab209cfc043a
Araein <75018476+Araein@users.noreply.github.com>
2021-07-01 19:49:07 +0200

    Update launch.sh

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -37,7 +37,7 @@ echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.tar.gz
     tar -xzvf phpMyAdmin-5.0.2-english.tar.gz
     mv -f phpMyAdmin-5.0.2-english/ /var/www/localhost/phpmyadmin
- #   rm -rf phpMyAdmin-5.0.2-english.tar.gz
+    rm -rf phpMyAdmin-5.0.2-english.tar.gz
 mv -f config.inc.php /var/www/localhost/phpmyadmin
 
 #wordpress 
