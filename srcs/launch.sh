32f884968622b345ffb1c9d75f18a79188c0d579
Araein <75018476+Araein@users.noreply.github.com>
2021-07-01 18:06:56 +0200

    Update launch.sh

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -9,7 +9,7 @@ service rsync -av /var/www/html /var/www/localhost
 
 openssl req -nodes -x509 -newkey  rsa:2048 -days 365 -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=127.0.0.1/CN=localhost" -keyout /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/certificate.localhost.crt 
 
-
+mv ./bonjour.php /var/www/localhost
 mv ./default etc/nginx/sites-available
 ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
 chown -R www-data /var/www/*
@@ -18,9 +18,18 @@ chmod -R 755 /var/www/*
 
 
 
+service mysql start
 
-mv ./bonjour.php /var/www/localhost
+#MARIADB
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost';" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+echo "update mysql.user set plugin = 'mysql_native_password' where user='wordpress_user';" | mysql -u root
 
+
+
+
+service mysql restart
 service php7.3-fpm start
 service nginx restart
 sleep infinity
