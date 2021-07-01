a4ed40b113b44ad530298c90596ffa365b7527c7
Araein <75018476+Araein@users.noreply.github.com>
2021-07-01 18:00:17 +0200

    Update launch.sh

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -10,10 +10,14 @@ service rsync -av /var/www/html /var/www/localhost
 openssl req -nodes -x509 -newkey  rsa:2048 -days 365 -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=127.0.0.1/CN=localhost" -keyout /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/certificate.localhost.crt 
 
 
-#mv ./default etc/nginx/sites-available
-#ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
+mv ./default etc/nginx/sites-available
+ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
+
+
+
+
 
 mv ./bonjour.php /var/www/localhost
 
