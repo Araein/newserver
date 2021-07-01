47aea9b6c569d68223bb952a7a1375479e293ca4
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 00:41:24 +0200

    Update config.inc.php

--- srcs/config.inc.php
+++ srcs/config.inc.php
@@ -30,7 +30,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = true;   /*true or false ??*/
+$cfg['Servers'][$i]['AllowNoPassword'] = false;   /*true or false ??*/
 
 /**
  * phpMyAdmin configuration storage settings.
