c23b7ada5c8fbf43d9efdafdd85ff31bd50a0888
Araein <75018476+Araein@users.noreply.github.com>
2021-07-01 03:13:58 +0200

    Update launch.sh

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -1,4 +1,12 @@
 
 service nginx start
 
+#ssl 
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=emma/CN=localhost"
+
+
+mv ./default etc/nginx/sites-available
+
+
 sleep infinity
