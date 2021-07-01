a70ae96032e79e3b6929e62f09edb3df63480af7
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 00:31:50 +0200

    Update launch.sh

--- srcs/launch.sh
+++ srcs/launch.sh
@@ -6,7 +6,6 @@ mkdir /etc/nginx/ssl
 mkdir /var/www/localhost
 
 service rsync -av /var/www/html /var/www/localhost
-
 openssl req -nodes -x509 -newkey  rsa:2048 -days 365 -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=127.0.0.1/CN=localhost" -keyout /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/certificate.localhost.crt 
 
 mv ./bonjour.php /var/www/localhost
@@ -14,9 +13,6 @@ mv ./default etc/nginx/sites-available
 ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
-
-
-
 
 service mysql start
 
@@ -55,16 +51,17 @@ mv -f config.inc.php /var/www/localhost/phpmyadmin
 
 wget https://wordpress.org/latest.tar.gz
 tar xvf latest.tar.gz
-    mv -f wordpress/ /var/www/localhost/wordpress
+rm -rf latest.tar.gz
+mv -f wordpress/ /var/www/localhost/wordpress
 mv ./wp-config.php /var/www/localhost/wordpress
 
 #chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
 #chown -R www-data:www-data /var/www/localhost/*
 #chmod -R 755 /var/www/*
 
-chown -R www-data:www-data /var/www/localhost && \
-		chmod -R 755 /var/www/localhost/wordpress && \
-		chmod -R 755 /var/www/localhost/phpmyadmin
+
+#chmod -R 755 /var/www/localhost/wordpress
+#chmod -R 755 /var/www/localhost/phpmyadmin
 
 service mysql restart
 service php7.3-fpm start
