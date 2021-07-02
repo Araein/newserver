f8325ff43c94bb1ae09ca53cfda2fecf7b1f3a7e
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 07:34:51 +0200

    Update and rename go.sh to gowithindex.sh

--- go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-docker stop myserver
-docker rm myserver
-docker rmi myserver
-docker build -t myserver . 
-docker run --env INDEX=off --rm --name myserver -d -p 80:80 -p 443:443 myserver
-docker exec -ti myserver bash
---
+++ gowithindex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+docker stop myserver
+docker rm myserver
+docker rmi myserver
+docker build -t myserver . 
+docker run --env --rm --name myserver -d -p 80:80 -p 443:443 myserver
+#docker exec -ti myserver bash
