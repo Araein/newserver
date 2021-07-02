5a23af2c821097ebee41949abb29b2e1deaf1f6e
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 07:09:22 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -6,4 +6,4 @@ docker rm myserver
 docker rmi myserver
 docker build -t myserver . 
 docker run --env INDEX=off --rm --name myserver -d -p 80:80 -p 443:443 myserver
-docker exec -ti myserver bash
+#docker exec -ti myserver bash
