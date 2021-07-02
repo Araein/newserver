f0a8ab2d3f2161c514b24f5b2396d92624369fff
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 07:37:08 +0200

    Create gowithoutindex.sh

---
+++ gowithoutindex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+docker stop myserver
+docker rm myserver
+docker rmi myserver
+sed -i 's/autoindex on;/autoindex off;/' ./srcs/default
+docker build -t myserver . 
+docker run --env --rm --name myserver -d -p 80:80 -p 443:443 myserver
+#docker exec -ti myserver bash
