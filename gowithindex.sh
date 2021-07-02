2939100d66272f51f4f6b24df46492517fde639c
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 11:23:58 +0200

    Update gowithindex.sh

--- gowithindex.sh
+++ gowithindex.sh
@@ -2,7 +2,7 @@
 
 docker stop myserver
 docker rm myserver
-docker rmi myserver
+#docker rmi myserver
 docker build -t myserver . 
 sed -i 's/autoindex off;/autoindex on;/' ./srcs/default
 docker run --env --rm --name myserver -d -p 80:80 -p 443:443 myserver
