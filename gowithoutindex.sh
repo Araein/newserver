8b350ac852407482a65e9130b4ab66a4a7230908
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 17:54:10 +0200

    Update gowithoutindex.sh

--- gowithoutindex.sh
+++ gowithoutindex.sh
@@ -6,5 +6,6 @@ docker rm myserver
 #docker rmi myserver
 sed -i 's/autoindex on;/autoindex off;/' ./srcs/default
 docker build -t myserver . 
+#-d detached -p expose -rm  automatically clean up the container and remove the file system when the container exits
 docker run --env --rm --name myserver -d -p 80:80 -p 443:443 myserver
 #docker exec -ti myserver bash
