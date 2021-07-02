f5e910d0d6e241c0d82bf052d267a887694220d7
Araein <75018476+Araein@users.noreply.github.com>
2021-07-02 06:35:30 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,10 +4,6 @@
 docker stop myserver
 docker rm myserver
 docker rmi myserver
-#docker build -t monimage .
-#docker run -d -p 80:80 --name=monconteneur monimage
-#docker exec -ti monconteneur bash
-
 docker build -t myserver . 
-docker run --rm --name myserver -d -p 80:80 -p 443:443 myserver
+docker run --env AUTOINDEX=on --rm --name myserver -d -p 80:80 -p 443:443 myserver
 docker exec -ti myserver bash
